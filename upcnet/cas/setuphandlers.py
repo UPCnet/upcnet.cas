7ca25d6424efe5f0832ca22fbb6f7838dcb718e9
victor.fernandez <victor.fernandez@6deaf63b-a510-49cf-aad4-8c651776ecce>
2009-10-19 11:44:08 +0000

    Paquet de personalització dels elements de UI del CAS UPC i script per afegir automàticament el plugin de CAS
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upcnet.cas@1364 6deaf63b-a510-49cf-aad4-8c651776ecce

---
+++ upcnet/cas/setuphandlers.py
@@ -0,0 +1,34 @@
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.utils import _createObjectByType
+
+from Products.CAS4PAS.CASAuthHelper import addCASAuthHelper 
+
+from plone.app.controlpanel.site import ISiteSchema
+
+from zope.component import getAdapters
+
+import transaction
+
+def setupVarious(context):
+    
+    # Ordinarily, GenericSetup handlers check for the existence of XML files.
+    # Here, we are not parsing an XML file, but we use this text file as a 
+    # flag to check that we actually meant for this import step to be run.
+    # The file is found in profiles/default.
+    
+    if context.readDataFile('upcnet.cas_various.txt') is None:
+        return
+
+    portal = context.getSite()
+    try:
+            addCASAuthHelper(portal.acl_users, "CASUPC", title="CASUPC")
+            portal.acl_users.CASUPC.login_url='https://cas.upc.edu/login'
+            portal.acl_users.CASUPC.logout_url='https://cas.upc.edu/logout'
+            portal.acl_users.CASUPC.validate_url='https://cas.upc.edu/validate'
+            plugin = portal.acl_users['CASUPC']
+
+            plugin.manage_activateInterfaces(['IAuthenticationPlugin','IChallengePlugin','IExtractionPlugin'])
+
+    except: 
+            pass
+
